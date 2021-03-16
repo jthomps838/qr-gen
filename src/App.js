5d79c9a04d5f38b047fca9e18f69670731222c04
Joseph Thompson <joe.thompson838@gmail.com>
2021-03-16 14:15:54 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import CodeGenerator from './components/QRCode'
+import styled from 'styled-components'
+
+const AppWrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	flex-direction: column;
+	align-items: center;
+	margin: 0 auto;
+`
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<AppWrapper>
+			<h1>QR Code Generator</h1>
+			<CodeGenerator />
+		</AppWrapper>
+	)
 }
 
-export default App;
+export default App
